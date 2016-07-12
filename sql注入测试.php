6c4750630b67c5f8b84be882b13a83e6e523f1ab
lufeirider <3020207744@qq.com>
2016-07-12 23:13:37 +0800

    Create sql注入测试.php

---
+++ sql注入测试.php
@@ -0,0 +1,23 @@
+<?php
+	$id = $_GET['uid'];
+	$conn=mysql_connect("localhost","root","");
+	mysql_select_db("guest");
+	$re=mysql_query("select * from admin where id=1");
+	$row=mysql_fetch_array($re);
+	echo $row["user"];
+?>
+
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) NOT NULL,
+  `user` varchar(10000) NOT NULL,
+  `pass` varchar(10000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 转存表中的数据 `admin`
+--
+
+INSERT INTO `admin` (`id`, `user`, `pass`) VALUES
+(1, 'lufei', '123456'),
+(2, 'hack', '123456');
