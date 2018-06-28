c4f3a26e903a1e9654039f406242b73647358c1c
lufeirider <3020207744@qq.com>
2018-06-28 17:11:26 +0800

    Update CSP.php

--- vul/CSP.php
+++ vul/CSP.php
@@ -9,3 +9,16 @@ alert(2);
 </script>
 
 <script src="x.js"></script>
+
+https://www.jianshu.com/p/f1de775bc43e
+<?php
+header("Content-Security-Policy: default-src 'self';img-src 'self' www.baidu.com");
+?>
+
+<script src="1.js">
+</script>
+<script>
+alert(2);
+</script>
+<img src="https://www.baidu.com/img/bd_logo1.png?where=super">
+<img src="1" onerror=alert(3)>
